799a1a3b6dc606f2ceecbb8d356ae75d1e49142b
VioletGiraffe <noreply@github.com>
2021-06-19 21:35:32 +0300

    noexcept

--- resize/cimageinterpolationkernel.h
+++ resize/cimageinterpolationkernel.h
@@ -7,10 +7,10 @@ template <typename CoeffType>
 class CImageInterpolationKernelBase
 {
 public:
-	virtual ~CImageInterpolationKernelBase() {}
+	virtual ~CImageInterpolationKernelBase() noexcept = default;
 
-	virtual CoeffType coeff(int x, int y) const = 0;
-	virtual int size() const = 0;
+	virtual CoeffType coeff(int x, int y) const noexcept = 0;
+	virtual int size() const noexcept = 0;
 };
 
 template <typename CoeffType>
@@ -21,23 +21,21 @@ public:
 		_kernel.resize(s);
 		for(int i = 0; i < _size; ++i)
 		{
-			_kernel[i].resize(s);
-			for(int k = 0; k < _size; ++k)
-				_kernel[i][k] = 0.0f;
+			_kernel[i].resize(s, CoeffType{0});
 		}
 	}
 
-	CoeffType coeff(int x, int y) const override
+	CoeffType coeff(int x, int y) const noexcept override
 	{
 		assert_debug_only(x < _size && y < _size); // Affects performance a whole lot! Don't change to assert_r
 		return _kernel[x][y];
 	}
 
-	int size() const override { return _size; }
+	int size() const noexcept override { return _size; }
 
 protected:
-	void normalizeKernel() {
-		CoeffType sum = CoeffType(0.0);
+	void normalizeKernel() noexcept {
+		CoeffType sum {0};
 		for (int i = 0; i < size(); ++i)
 			for (int k = 0; k < size(); ++k)
 				sum += _kernel[i][k];
@@ -51,25 +49,25 @@ protected:
 	std::vector<std::vector<CoeffType> > _kernel;
 };
 
-class CBicubicKernel : public CImageInterpolationKernel<float>
+class CBicubicKernel final : public CImageInterpolationKernel<float>
 {
 public:
 	CBicubicKernel(int s, float a);
 };
 
-class CTriangularKernel : public CImageInterpolationKernel<float>
+class CTriangularKernel final : public CImageInterpolationKernel<float>
 {
 public:
 	CTriangularKernel();
 };
 
-class CBellBicubicKernel : public CImageInterpolationKernel<float>
+class CBellBicubicKernel final : public CImageInterpolationKernel<float>
 {
 public:
 	explicit CBellBicubicKernel(int s);
 };
 
-class CLanczosKernel : public CImageInterpolationKernel<float>
+class CLanczosKernel final : public CImageInterpolationKernel<float>
 {
 public:
 	CLanczosKernel(int s, int a = 2);
