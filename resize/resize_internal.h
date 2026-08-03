3dc3f1805df961cf65202941add1b3d613323d3b
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-08-03 22:02:48 +0300

    Faster (?) clamp

--- resize/resize_internal.h
+++ resize/resize_internal.h
@@ -36,12 +36,7 @@ namespace ImageProcessing::Detail
 
 	[[nodiscard]] inline uint8_t clampToByte(float value) noexcept
 	{
-		if (value <= 0.0f) [[unlikely]]
-			return 0;
-
-		if (value >= 255.0f) [[unlikely]]
-			return 255;
-
+		value = std::min(std::max(value, 0.0f), 255.0f);
 		return static_cast<uint8_t>(value + 0.5f);
 	}
 
