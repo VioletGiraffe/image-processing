ae1f9db64e396391253cfeaf8d5e33be29090df8
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-08-05 23:43:52 +0300

    Struct member init

--- resize/cimageresizer.h
+++ resize/cimageresizer.h
@@ -13,14 +13,14 @@ namespace ImageProcessing
 	{
 		using DataPtr = std::conditional_t<ConstView, const void*, void*>;
 
-		uint64_t width;
-		uint64_t height;
-		uint8_t channels;
-		uint8_t bytesPerChannel;
-		uint8_t pixelStrideBytes;
-		size_t bytesPerLine;
+		uint64_t width = 0;
+		uint64_t height = 0;
+		uint8_t channels = 0;
+		uint8_t bytesPerChannel = 0;
+		uint8_t pixelStrideBytes = 0;
+		size_t bytesPerLine = 0;
 
-		DataPtr data;
+		DataPtr data = nullptr;
 
 		template <class T>
 		[[nodiscard]] inline auto* scanLine(uint64_t line) const noexcept
@@ -31,6 +31,7 @@ namespace ImageProcessing
 				return reinterpret_cast<T*>(reinterpret_cast<uint8_t*>(data) + static_cast<size_t>(line) * bytesPerLine);
 		}
 	};
+
 	struct Rect {
 		uint64_t left = 0, top = 0;
 		uint64_t w = 0, h = 0;
