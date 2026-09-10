2f89561152c4a42b15fbfe0be712e0f5dc865f2b
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2026-09-10 19:24:13 +0300

    CI/tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,1 @@
-#include "compiler/compiler_warnings_control.h"
-
-#define CATCH_CONFIG_MAIN
-DISABLE_COMPILER_WARNINGS
-#include "3rdparty/catch2/catch.hpp"
-RESTORE_COMPILER_WARNINGS
+#include "3rdparty/catch2/test_main.hpp"
